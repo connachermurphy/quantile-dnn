97648047c1cc9ddaee6a0c1fee2e88a0e97e8b70
Connor Murphy <connacher.murphy@gmail.com>
2024-02-13 16:05:01 -0600

    Add class for (smooth) check loss functions

--- code/quantile_dnn.py
+++ code/quantile_dnn.py
@@ -1,4 +1,6 @@
 import torch
+import torch.nn as nn
+
 
 def check_loss(input, target, tau, reduction: str = "mean"):
     error = target - input
@@ -7,9 +9,31 @@ def check_loss(input, target, tau, reduction: str = "mean"):
 
     return torch.mean(loss) if reduction == "mean" else torch.sum(loss)
 
+
 def smooth_check_loss(input, target, tau, alpha, reduction: str = "mean"):
     error = target - input
-    
+
     loss = tau * error + alpha * torch.log(1 + torch.exp(-error / alpha))
 
     return torch.mean(loss) if reduction == "mean" else torch.sum(loss)
+
+
+class CheckLoss(nn.Module):
+    def __init__(self, tau, reduction="mean"):
+        super().__init__()
+        self.tau = tau
+        self.reduction = reduction
+
+    def forward(self, input, target):
+        return check_loss(input, target, self.tau, self.reduction)
+
+
+class SmoothCheckLoss(nn.Module):
+    def __init__(self, tau, alpha, reduction="mean"):
+        super().__init__()
+        self.tau = tau
+        self.alpha = alpha
+        self.reduction = reduction
+
+    def forward(self, input, target):
+        return smooth_check_loss(input, target, self.tau, self.alpha, self.reduction)
